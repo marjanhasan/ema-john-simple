7906d5d9613135694000d5368a102358b7b91c86
marjanhasan <moonmarjanhasan@gmail.com>
2023-03-27 02:36:55 +0600

    cart added with data

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Product.css";
 const Product = (props) => {
-  const { img, seller, name, quantity, price } = props.product;
+  const { img, seller, name, ratings, price } = props.product;
   return (
     <div className="product">
       <img src={img} alt="" />
-      <h6>{name}</h6>
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings}Star</p>
+      </div>
+      <button className="btn-cart">Add to Cart</button>
     </div>
   );
 };
