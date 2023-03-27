db5c9549fcb5e38c8e57eabb3f3b429e26e7ae17
marjanhasan <moonmarjanhasan@gmail.com>
2023-03-27 22:07:36 +0600

    added cart component and calculation feature

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,18 +5,22 @@ const Cart = ({ cart }) => {
   // const cart = props.cart; option 1
   // const { cart } = props;  option 2
   // option 3 hoilo props er jaygay { cart } likhe dewa
-  let total = 0;
-  for (const prod of cart) {
-    total += prod.price;
+  let totalPrice = 0;
+  let totalShipping = 0;
+  for (const product of cart) {
+    totalPrice += product.price;
+    totalShipping += product.shipping;
   }
+  const tax = (totalPrice * 7) / 100;
+  const grandTotal = totalPrice + totalShipping + tax;
   return (
     <div className="cart">
       <h4>order summary</h4>
       <p>Selected Items: {cart.length}</p>
-      <p>Total Price: {total}</p>
-      <p>Total Shipping: </p>
-      <p>Tax: </p>
-      <h6>Grand Total: </h6>
+      <p>Total Price: ${totalPrice}</p>
+      <p>Shipping: ${totalShipping}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
     </div>
   );
 };
